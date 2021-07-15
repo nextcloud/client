uusijani/desktop

Iteration: Added e2eesize into database.
Signed-off-by: allexzander <blackslayer4@gmail.com>

src/common/syncjournalfilerecord.h
@@ -65,6 +65,7 @@ public:
     QByteArray _etag;
     QByteArray _fileId;
     qint64 _fileSize = 0;
+    qint64 _fileSizeE2EE = 0;
     RemotePermissions _remotePerm;
     bool _serverHasIgnoredFiles = false;
     QByteArray _checksumHeader;

src/libsync/syncfileitem.h
@@ -202,6 +202,8 @@ public:
             && !(_instruction == CSYNC_INSTRUCTION_CONFLICT && _status == SyncFileItem::Success);
     }
 
+    qint64 sizeForVfsPlaceholder() const;
+
     // Variables useful for everybody
 
     /** The syncfolder-relative filesystem path that the operation is about
@@ -261,6 +263,7 @@ public:
     time_t _modtime = 0;
     QByteArray _etag;
     qint64 _size = 0;
+    qint64 _sizeE2ee = 0;
     quint64 _inode = 0;
     QByteArray _fileId;
 

nextcloud/client

src/common/syncjournalfilerecord.h
@@ -65,6 +65,7 @@ public:
     QByteArray _etag;
     QByteArray _fileId;
     qint64 _fileSize = 0;
+    qint64 _fileSizeNonE2EE = 0;
     RemotePermissions _remotePerm;
     bool _serverHasIgnoredFiles = false;
     QByteArray _checksumHeader;

src/libsync/syncfileitem.h
@@ -202,6 +202,8 @@ public:
             && !(_instruction == CSYNC_INSTRUCTION_CONFLICT && _status == SyncFileItem::Success);
     }
 
+    qint64 sizeForVfsPlaceholder() const;
+
     // Variables useful for everybody
 
     /** The syncfolder-relative filesystem path that the operation is about
@@ -261,6 +263,7 @@ public:
     time_t _modtime = 0;
     QByteArray _etag;
     qint64 _size = 0;
+    qint64 _sizeNonE2EE = 0;
     quint64 _inode = 0;
     QByteArray _fileId;
 
@@ -274,6 +277,7 @@ public:
 
     // The size and modtime of the file getting overwritten (on the disk for downloads, on the server for uploads).
     qint64 _previousSize = 0;
+    qint64 _previousSizeNonE2EE = 0;
     time_t _previousModtime = 0;
 
     QString _directDownloadUrl;